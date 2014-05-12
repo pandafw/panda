155df7bced5707cc8159c2233c5516a3e1a8abed
Frank Wang <yf.frank.wang@outlook.com>
2014-05-12 23:46:37 +0900

    update getRequestURI and add getServletPath

--- src/main/java/panda/servlet/HttpServlets.java
+++ src/main/java/panda/servlet/HttpServlets.java
@@ -140,12 +140,51 @@ public class HttpServlets {
 		if (Strings.isEmpty(uri)) {
 			uri = request.getRequestURI();
 		}
-		else {
-			uri = URLHelper.decodeURL(uri);
-		}
+		uri = URLHelper.decodeURL(uri);
 		return uri;
 	}
 	
+	/**
+	 * Retrieves the current request servlet path. Deals with differences between servlet specs (2.2
+	 * vs 2.3+)
+	 * 
+	 * @param request the request
+	 * @return the servlet path
+	 */
+	public static String getServletPath(HttpServletRequest request) {
+		String servletPath = (String)request.getAttribute(INCLUDE_SERVLET_PATH_ATTRIBUTE);
+		if (Strings.isEmpty(servletPath)) {
+			servletPath = request.getServletPath();
+		}
+
+		String requestUri = (String)request.getAttribute(FORWARD_REQUEST_URI_ATTRIBUTE);
+		if (Strings.isEmpty(requestUri)) {
+			requestUri = request.getRequestURI();
+		}
+		
+		// Detecting other characters that the servlet container cut off (like anything after ';')
+		if (requestUri != null && servletPath != null && !requestUri.endsWith(servletPath)) {
+			int pos = requestUri.indexOf(servletPath);
+			if (pos > -1) {
+				servletPath = requestUri.substring(requestUri.indexOf(servletPath));
+			}
+		}
+
+		if (null != servletPath && !"".equals(servletPath)) {
+			return servletPath;
+		}
+
+		int startIndex = request.getContextPath().equals("") ? 0 : request.getContextPath().length();
+		int endIndex = request.getPathInfo() == null ? requestUri.length() : requestUri.lastIndexOf(request.getPathInfo());
+
+		if (startIndex > endIndex) { // this should not happen
+			endIndex = startIndex;
+		}
+
+		requestUri = requestUri.substring(startIndex, endIndex);
+		return URLHelper.decodeURL(requestUri);
+	}
+
 	/**
 	 * @param request request
 	 * @return request query string
