2cc7640249092660342e34126f67de8d81dc276c
Frank Wang <yf.frank.wang@outlook.com>
2014-11-06 21:09:33 +0900

    ioc update

--- src/main/java/panda/mvc/ioc/provider/AbstractIocProvider.java
+++ src/main/java/panda/mvc/ioc/provider/AbstractIocProvider.java
@@ -1,12 +1,11 @@
 package panda.mvc.ioc.provider;
 
-import panda.ioc.Ioc;
 import panda.ioc.IocLoader;
 import panda.ioc.impl.DefaultIoc;
 import panda.mvc.IocProvider;
 
 public abstract class AbstractIocProvider implements IocProvider {
-	protected Ioc create(IocLoader loader) {
+	protected DefaultIoc create(IocLoader loader) {
 		return new DefaultIoc(loader);
 	}
 
