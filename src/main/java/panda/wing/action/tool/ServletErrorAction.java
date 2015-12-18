6efd10b8a91669c9063e21c610ce1b533fb4e59d
Frank Wang <yf.frank.wang@outlook.com>
2015-12-18 21:41:10 +0900

    update error-page

--- src/main/java/panda/wing/action/tool/ServletErrorAction.java
+++ src/main/java/panda/wing/action/tool/ServletErrorAction.java
@@ -1,24 +1,32 @@
 package panda.wing.action.tool;
 
-import panda.ioc.annotation.IocInject;
+import panda.io.FileNames;
+import panda.lang.Numbers;
 import panda.mvc.annotation.At;
-import panda.mvc.annotation.view.Ok;
-import panda.mvc.view.ftl.FreemarkerHelper;
+import panda.mvc.view.FreemarkerView;
+import panda.mvc.view.SitemeshFreemarkerView;
+import panda.servlet.HttpServlets;
 import panda.wing.action.AbstractAction;
 
-@At
+@At("/servlet-error")
 public class ServletErrorAction extends AbstractAction {
 
-	@IocInject
-	FreemarkerHelper freemarker;
+	private static final String TPL = "/panda/mvc/view/servlet-error.ftl";
 	
-	@At("/panda/mvc/view/servlet-error.ftl")
-	@Ok("ftl:/panda/mvc/view/servlet-error.ftl")
-	public void ftl() {
-	}
+	@At("*")
+	public Object execute(String error) {
+		Object sc = getRequest().getAttribute(HttpServlets.ERROR_STATUS_CODE_ATTRIBUTE);
+		if (sc == null) {
+			sc = Numbers.toInt(FileNames.getBaseName(error));
+			if (sc != null) {
+				getRequest().setAttribute(HttpServlets.ERROR_STATUS_CODE_ATTRIBUTE, sc);
+			}
+		}
 
-	@At("/panda/mvc/view/servlet-error.sftl")
-	@Ok("sftl:/panda/mvc/view/servlet-error.ftl")
-	public void sftl() {
+		String ext = FileNames.getExtension(error);
+		if ("sftl".equalsIgnoreCase(ext)) {
+			return new SitemeshFreemarkerView(TPL);
+		}
+		return new FreemarkerView(TPL);
 	}
 }
