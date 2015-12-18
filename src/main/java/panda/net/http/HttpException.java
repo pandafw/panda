113328232553d4e992e0cfbafb38bffb2d6a6611
Frank Wang <yf.frank.wang@outlook.com>
2015-12-18 23:44:50 +0900

    add saveException()

--- src/main/java/panda/net/http/HttpException.java
+++ src/main/java/panda/net/http/HttpException.java
@@ -1,8 +1,15 @@
 package panda.net.http;
 
+
 public class HttpException extends RuntimeException {
 
 	private static final long serialVersionUID = 4035188583429445028L;
+
+	public static final HttpException BAD_REQUEST = new HttpException(HttpStatus.SC_BAD_REQUEST);
+	public static final HttpException FORBIDDEN = new HttpException(HttpStatus.SC_FORBIDDEN);
+	public static final HttpException NOT_FOUND = new HttpException(HttpStatus.SC_NOT_FOUND);
+	public static final HttpException SERVER_ERROR = new HttpException(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+	public static final HttpException BAD_GATEWAY = new HttpException(HttpStatus.SC_BAD_GATEWAY);
 
 	private int status;
 
