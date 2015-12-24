c9f9ba13e0e822c3e398527ac81767cab0b3b121
Frank Wang <yf.frank.wang@outlook.com>
2015-12-24 15:45:32 +0900

    remote unused property of IUser

--- src/main/java/panda/wing/auth/IUser.java
+++ src/main/java/panda/wing/auth/IUser.java
@@ -5,12 +5,6 @@ import java.util.List;
 public interface IUser {
 	public Long getId();
 
-	public void setId(Long id);
-
-	public String getName();
-
-	public String getPassword();
-
 	public List<String> getPermits();
 	
 	public Long getLoginTime();
