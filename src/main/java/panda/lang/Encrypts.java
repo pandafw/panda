10ae3a36e645ac61dde8018abfcb8cb0080bf244
Frank Wang <yf.frank.wang@outlook.com>
2016-01-04 21:01:31 +0900

    update

--- src/main/java/panda/lang/Encrypts.java
+++ src/main/java/panda/lang/Encrypts.java
@@ -25,38 +25,46 @@ public class Encrypts {
 	public static final String RSA = "RSA";
 	
 	//------------------------------------------------------------------------------
-	public static final String DEFAULT_TRANSFORM = Encrypts.Blowfish;
+	public static final String DEFAULT_CIPHER = Encrypts.Blowfish;
 	public static final String DEFAULT_KEY = "== Panda Java ==";
 	public static final byte[] DEFAULT_KEY_BYTES = DEFAULT_KEY.getBytes();
 
 	//------------------------------------------------------------------------------
+	public static String trimKey(String key) {
+		return Strings.center(key, 16, '-');
+	}
+	
+	public static byte[] keyBytes(String key) {
+		return Strings.getBytesUtf8(key);
+	}
+	
 	public static String encrypt(String text) {
-		return encrypt(text, null, DEFAULT_TRANSFORM);
+		return encrypt(text, null, DEFAULT_CIPHER);
 	}
 
 	public static String decrypt(String text) {
-		return decrypt(text, null, DEFAULT_TRANSFORM);
+		return decrypt(text, null, DEFAULT_CIPHER);
 	}
 	
 	public static String encrypt(String text, String key) {
-		return encrypt(text, key, DEFAULT_TRANSFORM);
+		return encrypt(text, key, DEFAULT_CIPHER);
 	}
 
 	public static String decrypt(String text, String key) {
-		return decrypt(text, key, DEFAULT_TRANSFORM);
+		return decrypt(text, key, DEFAULT_CIPHER);
 	}
 	
-	public static String encrypt(String text, String key, String transform) {
+	public static String encrypt(String text, String key, String cipher) {
 		byte[] t = Strings.getBytesUtf8(text);
-		byte[] k = key == null ? DEFAULT_KEY_BYTES : Strings.getBytesUtf8(key);
-		byte[] encrypted = encrypt(t, k, transform);
+		byte[] k = key == null ? DEFAULT_KEY_BYTES : keyBytes(key);
+		byte[] encrypted = encrypt(t, k, cipher);
 		return Strings.newStringUtf8(Base64.encodeBase64(encrypted, false, false));
 	}
 
-	public static String decrypt(String text, String key, String transform) {
+	public static String decrypt(String text, String key, String cipher) {
 		byte[] t = Base64.decodeBase64(text);
-		byte[] k = key == null ? DEFAULT_KEY_BYTES : Strings.getBytesUtf8(key);
-		byte[] decrypted = decrypt(t, k, transform);
+		byte[] k = key == null ? DEFAULT_KEY_BYTES : keyBytes(key);
+		byte[] decrypted = decrypt(t, k, cipher);
 		return Strings.newStringUtf8(decrypted);
 	}
 
@@ -70,12 +78,12 @@ public class Encrypts {
 		return "PBEWith" + digest + "And" + encryption;
 	}
 	
-	public static byte[] encrypt(byte[] text, byte[] key, String transform) {
+	public static byte[] encrypt(byte[] text, byte[] key, String cipher) {
 		try {
-			SecretKeySpec sksSpec = new SecretKeySpec(key, transform);
-			Cipher cipher = Cipher.getInstance(transform);
-			cipher.init(Cipher.ENCRYPT_MODE, sksSpec);
-			byte[] encrypted = cipher.doFinal(text);
+			SecretKeySpec sksSpec = new SecretKeySpec(key, cipher);
+			Cipher c = Cipher.getInstance(cipher);
+			c.init(Cipher.ENCRYPT_MODE, sksSpec);
+			byte[] encrypted = c.doFinal(text);
 			return encrypted;
 		}
 		catch (Exception e) {
@@ -83,11 +91,11 @@ public class Encrypts {
 		}
 	}
 
-	public static byte[] encrypt(byte[] text, SecretKey key, String transform) {
+	public static byte[] encrypt(byte[] text, SecretKey key, String cipher) {
 		try {
-			Cipher cipher = Cipher.getInstance(transform);
-			cipher.init(Cipher.ENCRYPT_MODE, key);
-			byte[] encrypted = cipher.doFinal(text);
+			Cipher c = Cipher.getInstance(cipher);
+			c.init(Cipher.ENCRYPT_MODE, key);
+			byte[] encrypted = c.doFinal(text);
 			return encrypted;
 		}
 		catch (Exception e) {
@@ -95,12 +103,12 @@ public class Encrypts {
 		}
 	}
 
-	public static byte[] decrypt(byte[] data, byte[] key, String transform) {
+	public static byte[] decrypt(byte[] data, byte[] key, String cipher) {
 		try {
-			SecretKeySpec sksSpec = new SecretKeySpec(key, transform);
-			Cipher cipher = Cipher.getInstance(transform);
-			cipher.init(Cipher.DECRYPT_MODE, sksSpec);
-			byte[] decrypted = cipher.doFinal(data);
+			SecretKeySpec sksSpec = new SecretKeySpec(key, cipher);
+			Cipher c = Cipher.getInstance(cipher);
+			c.init(Cipher.DECRYPT_MODE, sksSpec);
+			byte[] decrypted = c.doFinal(data);
 			return decrypted;
 		}
 		catch (Exception e) {
@@ -108,11 +116,11 @@ public class Encrypts {
 		}
 	}
 
-	public static byte[] decrypt(byte[] data, SecretKey key, String transform) {
+	public static byte[] decrypt(byte[] data, SecretKey key, String cipher) {
 		try {
-			Cipher cipher = Cipher.getInstance(transform);
-			cipher.init(Cipher.DECRYPT_MODE, key);
-			byte[] decrypted = cipher.doFinal(data);
+			Cipher c = Cipher.getInstance(cipher);
+			c.init(Cipher.DECRYPT_MODE, key);
+			byte[] decrypted = c.doFinal(data);
 			return decrypted;
 		}
 		catch (Exception e) {
