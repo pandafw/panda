58748a8fba158adc0b2f5ed42a34a57f82b311cc
Frank Wang <yf.frank.wang@outlook.com>
2016-02-05 21:30:34 +0900

    add getSubdomain()

--- src/main/java/panda/net/Inets.java
+++ src/main/java/panda/net/Inets.java
@@ -37,7 +37,12 @@ public class Inets {
 			return false;
 		}
 	}
-	
+
+	/**
+	 * @param size size
+	 * @param time ms time
+	 * @return display speed string
+	 */
 	public static String toSpeedString(long size, long time) {
 		double sec = (double)time / DateTimes.MS_SECOND; 
 		if (sec <= 0) {
@@ -52,5 +57,25 @@ public class Inets {
 	 */
 	public static String toSpeedString(double speed) {
 		return Files.toDisplaySize(speed) + "/s";
+	}
+
+	/**
+	 * get sub domain
+	 * @param domain domain
+	 * @return sub domain
+	 */
+	public static String getSubdomain(String domain) {
+		if (Strings.isEmpty(domain)) {
+			return domain;
+		}
+		
+		int i = domain.lastIndexOf('.');
+		if (i > 0) {
+			int j = domain.lastIndexOf('.', i - 1);
+			if (j > 0) {
+				return domain.substring(0, j);
+			}
+		}
+		return Strings.EMPTY;
 	}
 }
