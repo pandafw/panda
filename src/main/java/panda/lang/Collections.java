ff5018dde8c01f2bac4263426e907514b742cd28
Frank Wang <yf.frank.wang@outlook.com>
2016-02-16 21:33:22 +0900

    add firstKey(Map), firstValue(Map), firstEntry(Map)

--- src/main/java/panda/lang/Collections.java
+++ src/main/java/panda/lang/Collections.java
@@ -1,6 +1,8 @@
 package panda.lang;
 
-import java.lang.reflect.Array;
+import panda.lang.collection.CaseInsensitiveMap;
+import panda.lang.collection.SafeMap;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
@@ -23,9 +25,6 @@ import java.util.Set;
 import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
-
-import panda.lang.collection.CaseInsensitiveMap;
-import panda.lang.collection.SafeMap;
 
 /**
  * @author yf.frank.wang@gmail.com
@@ -338,195 +337,44 @@ public abstract class Collections {
 	}
 
 	/**
-	 * Returns the <code>index</code>-th value in <code>object</code>, throwing
-	 * <code>IndexOutOfBoundsException</code> if there is no such element or
-	 * <code>IllegalArgumentException</code> if <code>object</code> is not an instance of one of the
-	 * supported types.
-	 * <p>
-	 * The supported types, and associated semantics are:
-	 * <ul>
-	 * <li>Map -- the value returned is the <code>Map.Entry</code> in position <code>index</code> in
-	 * the map's <code>entrySet</code> iterator, if there is such an entry.</li>
-	 * <li>List -- this method is equivalent to the list's get method.</li>
-	 * <li>Array -- the <code>index</code>-th array entry is returned, if there is such an entry;
-	 * otherwise an <code>IndexOutOfBoundsException</code> is thrown.</li>
-	 * <li>Collection -- the value returned is the <code>index</code>-th object returned by the
-	 * collection's default iterator, if there is such an element.</li>
-	 * <li>Iterator or Enumeration -- the value returned is the <code>index</code>-th object in the
-	 * Iterator/Enumeration, if there is such an element. The Iterator/Enumeration is advanced to
-	 * <code>index</code> (or to the end, if <code>index</code> exceeds the number of entries) as a
-	 * side effect of this method.</li>
-	 * </ul>
-	 * 
-	 * @param object the object to get a value from
-	 * @param index the index to get
-	 * @return the object at the specified index
-	 * @throws IndexOutOfBoundsException if the index is invalid
-	 * @throws IllegalArgumentException if the object type is invalid
+	 * return first key of map
+	 * @param map
+	 * @return first key
 	 */
-	public static Object get(Object object, int index) {
-		if (index < 0) {
-			throw new IndexOutOfBoundsException("Index cannot be negative: " + index);
+	public static <K> K firstKey(Map<K, ?> map) {
+		if (isEmpty(map)) {
+			return null;
 		}
-		if (object instanceof Map) {
-			Map map = (Map)object;
-			Iterator iterator = map.entrySet().iterator();
-			return get(iterator, index);
-		}
-		else if (object instanceof List) {
-			return ((List)object).get(index);
-		}
-		else if (object instanceof Object[]) {
-			return ((Object[])object)[index];
-		}
-		else if (object instanceof Iterator) {
-			Iterator it = (Iterator)object;
-			while (it.hasNext()) {
-				index--;
-				if (index == -1) {
-					return it.next();
-				}
-				else {
-					it.next();
-				}
-			}
-			throw new IndexOutOfBoundsException("Entry does not exist: " + index);
-		}
-		else if (object instanceof Collection) {
-			Iterator iterator = ((Collection)object).iterator();
-			return get(iterator, index);
-		}
-		else if (object instanceof Enumeration) {
-			Enumeration it = (Enumeration)object;
-			while (it.hasMoreElements()) {
-				index--;
-				if (index == -1) {
-					return it.nextElement();
-				}
-				else {
-					it.nextElement();
-				}
-			}
-			throw new IndexOutOfBoundsException("Entry does not exist: " + index);
-		}
-		else if (object == null) {
-			throw new IllegalArgumentException("Unsupported object type: null");
-		}
-		else {
-			try {
-				return Array.get(object, index);
-			}
-			catch (IllegalArgumentException ex) {
-				throw new IllegalArgumentException("Unsupported object type: "
-						+ object.getClass().getName());
-			}
-		}
+
+		return map.keySet().iterator().next();
 	}
 
 	/**
-	 * Gets the size of the collection/iterator specified.
-	 * <p>
-	 * This method can handles objects as follows
-	 * <ul>
-	 * <li>Collection - the collection size
-	 * <li>Map - the map size
-	 * <li>Array - the array size
-	 * <li>Iterator - the number of elements remaining in the iterator
-	 * <li>Enumeration - the number of elements remaining in the enumeration
-	 * </ul>
-	 * 
-	 * @param object the object to get the size of
-	 * @return the size of the specified collection
-	 * @throws IllegalArgumentException thrown if object is not recognised or null
+	 * return first value of map
+	 * @param map
+	 * @return first value
 	 */
-	public static int size(Object object) {
-		int total = 0;
-		if (object instanceof Map) {
-			total = ((Map)object).size();
+	public static <V> V firstValue(Map<?, V> map) {
+		if (isEmpty(map)) {
+			return null;
 		}
-		else if (object instanceof Collection) {
-			total = ((Collection)object).size();
-		}
-		else if (object instanceof Object[]) {
-			total = ((Object[])object).length;
-		}
-		else if (object instanceof Iterator) {
-			Iterator it = (Iterator)object;
-			while (it.hasNext()) {
-				total++;
-				it.next();
-			}
-		}
-		else if (object instanceof Enumeration) {
-			Enumeration it = (Enumeration)object;
-			while (it.hasMoreElements()) {
-				total++;
-				it.nextElement();
-			}
-		}
-		else if (object == null) {
-			throw new IllegalArgumentException("Unsupported object type: null");
-		}
-		else {
-			try {
-				total = Array.getLength(object);
-			}
-			catch (IllegalArgumentException ex) {
-				throw new IllegalArgumentException("Unsupported object type: "
-						+ object.getClass().getName());
-			}
-		}
-		return total;
+
+		return map.values().iterator().next();
 	}
 
 	/**
-	 * Checks if the specified collection/array/iterator is empty.
-	 * <p>
-	 * This method can handles objects as follows
-	 * <ul>
-	 * <li>Collection - via collection isEmpty
-	 * <li>Map - via map isEmpty
-	 * <li>Array - using array size
-	 * <li>Iterator - via hasNext
-	 * <li>Enumeration - via hasMoreElements
-	 * </ul>
-	 * <p>
-	 * Note: This method is named to avoid clashing with {@link #isEmpty(Collection)}.
-	 * 
-	 * @param object the object to get the size of, not null
-	 * @return true if empty
-	 * @throws IllegalArgumentException thrown if object is not recognised or null
+	 * return first entry of map
+	 * @param map
+	 * @return first entry
 	 */
-	public static boolean sizeIsEmpty(Object object) {
-		if (object instanceof Collection) {
-			return ((Collection)object).isEmpty();
+	public static <K, V> Entry<K, V> firstEntry(Map<K, V> map) {
+		if (isEmpty(map)) {
+			return null;
 		}
-		else if (object instanceof Map) {
-			return ((Map)object).isEmpty();
-		}
-		else if (object instanceof Object[]) {
-			return ((Object[])object).length == 0;
-		}
-		else if (object instanceof Iterator) {
-			return ((Iterator)object).hasNext() == false;
-		}
-		else if (object instanceof Enumeration) {
-			return ((Enumeration)object).hasMoreElements() == false;
-		}
-		else if (object == null) {
-			throw new IllegalArgumentException("Unsupported object type: null");
-		}
-		else {
-			try {
-				return Array.getLength(object) == 0;
-			}
-			catch (IllegalArgumentException ex) {
-				throw new IllegalArgumentException("Unsupported object type: "
-						+ object.getClass().getName());
-			}
-		}
-	}
 
+		return map.entrySet().iterator().next();
+	}
+	
 	// -----------------------------------------------------------------------
 	/**
 	 * Null-safe check if the specified collection is empty.
