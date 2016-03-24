d048141445c2e862e7309ac309f518c4d26fa924
Frank Wang <yf.frank.wang@outlook.com>
2016-03-24 10:47:05 +0900

    update ptrigger

--- src/main/panda/js/panda.js
+++ src/main/panda/js/panda.js
@@ -2633,29 +2633,30 @@ s_setbase({
 		option = $.extend({ 'icon' : 'fa fa-remove' }, option);
 		return this.each(function() {
 			var $t = $(this);
-			var f = option.onclick || $t.data('trigger');
-			if (f == 'false') {
+			var f = option.onclick || $t.data('ptrigger');
+			if (!f || f == 'false') {
 				return;
 			}
 
-			var $i = $('<i class="p-trigger ' + option.icon + '"></i>');
-			$i.insertAfter($t);
-
-			if (f && f != "true") {
-				panda_call(f, $t.get(0));
-			}
-			else {
-				$i.click(function() {
-					$t.val('');
-				});
-			}
+			var i = option.icon || $t.data('ptrigger-icon');
+			var $i = $('<i class="p-trigger ' + i + '"></i>');
+			$t.addClass('p-has-trigger');
+			$i.insertAfter($t)
+			  .click(function() {
+					if (f && f != "true" && f != true) {
+						panda_call(f, $t.get(0));
+					}
+					else {
+						$t.val('');
+					}
+			  });
 		});
 	};
 	
 	// DATA-API
 	// ==================
 	$(window).on('load', function () {
-		$('[data-trigger]').ptrigger();
+		$('[data-ptrigger]').ptrigger();
 	});
 })();
 (function($) {
