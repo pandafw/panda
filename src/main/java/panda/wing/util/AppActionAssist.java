3a8280e8987f281c9f6768e0042f723da639e067
Frank Wang <yf.frank.wang@outlook.com>
2016-03-30 10:37:24 +0900

    move TemplateMail to AppActionAssist

--- src/main/java/panda/wing/util/AppActionAssist.java
+++ src/main/java/panda/wing/util/AppActionAssist.java
@@ -1,9 +1,16 @@
 package panda.wing.util;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 import java.util.Map.Entry;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
 
 import panda.bind.json.JsonObject;
 import panda.bind.json.JsonSerializer;
@@ -11,6 +18,9 @@ import panda.cast.Castors;
 import panda.ioc.Scope;
 import panda.ioc.annotation.IocBean;
 import panda.ioc.annotation.IocInject;
+import panda.lang.Arrays;
+import panda.lang.Charsets;
+import panda.lang.Exceptions;
 import panda.lang.Locales;
 import panda.lang.Numbers;
 import panda.lang.Strings;
@@ -24,6 +34,7 @@ import panda.mvc.util.PermissionProvider;
 import panda.mvc.util.ServletUrlBuilder;
 import panda.mvc.util.StateProvider;
 import panda.mvc.view.ftl.FreemarkerHelper;
+import panda.net.SendMail;
 import panda.wing.auth.AuthHelper;
 import panda.wing.auth.IUser;
 import panda.wing.constant.RC;
@@ -390,4 +401,145 @@ public class AppActionAssist extends ActionAssist implements PermissionProvider 
 		getState().saveState("limit", pager.getLimit());
 	}
 	
+	//-------------------------------------------------------------
+	// Template mail
+	//
+	public void addEmailCc(Email email, String cc) throws EmailException {
+		for (String s : splitMail(cc)) {
+			email.addCc(s);
+		}
+	}
+	
+	public void addEmailBcc(Email email, String bcc) throws EmailException {
+		for (String s : splitMail(bcc)) {
+			email.addBcc(s);
+		}
+	}
+	
+	public Collection<String> splitMail(String str) {
+		Set<String> ms = new HashSet<String>();
+		
+		String[] ss = Strings.split(str, " \r\n\t,;\u3000");
+		if (ss != null) {
+			ms.addAll(Arrays.asList(ss));
+		}
+
+		return ms; 
+	}
+
+
+	/**
+	 * send email
+	 * @param email email
+	 * @param name template name
+	 * @param context context
+	 */
+	public void sendTemplateMail(Email email, String name, Object context) throws EmailException {
+		String subject = "";
+		String content;
+		try {
+			content = freemarker.execTemplate(name, context);
+		}
+		catch (Exception e) {
+			throw Exceptions.wrapThrow(e);
+		}
+
+		int cr = content.indexOf('\n');
+		if (cr > 0) {
+			subject = Strings.strip(content.substring(0, cr));
+			content = content.substring(cr + 1);
+		}
+
+		email.setSubject(subject);
+		if (email instanceof HtmlEmail) {
+			((HtmlEmail)email).setHtmlMsg(content);
+		}
+		else {
+			email.setMsg(content);
+		}
+
+		sendMail(email);
+	}
+
+	/**
+	 * send email
+	 * @param email email
+	 * @param name template name
+	 */
+	public void sendTemplateMail(Email email, String name) throws EmailException {
+		sendTemplateMail(email, name, null);
+	}
+	
+	/**
+	 * send email
+	 * @param email email
+	 * @throws EmailException if an email error occurs
+	 */
+	public void sendMail(Email email) throws EmailException {
+		if (getMailSettingAsBoolean(SC.MAIL_DEBUG, false)) {
+			email.setDebug(true);
+		}
+
+		String charset = getMailSetting(SC.MAIL_CHARSET, Charsets.UTF_8);
+		if (Strings.isNotEmpty(charset)) {
+			email.setCharset(charset);
+		}
+
+		email.setFrom(getMailSetting(SC.MAIL_FROM_MAIL, null), getMailSetting(SC.MAIL_FROM_NAME, null));
+
+		String host = getMailSetting(SC.MAIL_SMTP_HOST, null);
+		if (Strings.isNotEmpty(host)) {
+			email.setHostName(host);
+		}
+		
+		int port = getMailSettingAsInt(SC.MAIL_SMTP_PORT, 0);
+		if (port > 0) {
+			email.setSmtpPort(port);
+		}
+
+		if (getMailSettingAsBoolean(SC.MAIL_SMTP_SSL, false)) {
+			email.setSSL(true);
+			if (port > 0) {
+				email.setSslSmtpPort(String.valueOf(port));
+			}
+		}
+		
+		email.setTLS(getMailSettingAsBoolean(SC.MAIL_SMTP_TLS, false));
+		
+		String username = getMailSetting(SC.MAIL_SMTP_USER, null);
+		if (Strings.isNotEmpty(username)) {
+			email.setAuthentication(username, getMailSetting(SC.MAIL_SMTP_PASSWORD, ""));
+		}
+		
+		String bounce = getMailSetting(SC.MAIL_SMTP_BOUNCE, null);
+		if (Strings.isNotEmpty(bounce)) {
+			email.setBounceAddress(bounce);
+		}
+
+		SendMail.send(email);
+	}
+	
+	protected String getMailSetting(String key, String def) {
+		String s = context.getText().getText(key, null);
+		if (Strings.isNotEmpty(s)) {
+			return s;
+		}
+		return settings.getProperty(key, def);
+	}
+	
+	protected boolean getMailSettingAsBoolean(String key, boolean def) {
+		String v = Strings.stripToNull(getMailSetting(key, null));
+		if (Strings.isEmpty(v)) {
+			return def;
+		}
+		return (Boolean)Castors.scast(v, boolean.class);
+	}
+	
+	protected int getMailSettingAsInt(String key, int def) {
+		String v = Strings.stripToNull(getMailSetting(key, null));
+		if (Strings.isEmpty(v)) {
+			return def;
+		}
+		return (Integer)Castors.scast(v, int.class);
+	}
 }
