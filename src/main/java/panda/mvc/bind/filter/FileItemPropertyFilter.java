7c322f015b3936eaa1f63135021cda61c0f6a9e4
Frank Wang <yf.frank.wang@outlook.com>
2016-04-01 09:15:38 +0900

    fix javadoc

--- src/main/java/panda/mvc/bind/filter/FileItemPropertyFilter.java
+++ src/main/java/panda/mvc/bind/filter/FileItemPropertyFilter.java
@@ -7,8 +7,6 @@ import panda.bind.filter.IncludePropertyFilter;
 public class FileItemPropertyFilter extends IncludePropertyFilter {
 	/**
 	 * Constructor
-	 * @param shortName shortName
-	 * 
 	 */
 	public FileItemPropertyFilter() {
 		includes.add("id");
