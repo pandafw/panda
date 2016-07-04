352c4fbcd3eeb51c79d158e6e3b5bb74abc934d5
Frank Wang <yf.frank.wang@outlook.com>
2016-07-04 09:23:40 +0900

    add QueryerQx

---
+++ src/main/java/panda/mvc/bean/QueryerOx.java
@@ -0,0 +1,27 @@
+package panda.mvc.bean;
+
+
+public class QueryerOx extends Queryer {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final Pager DUMMY = new Pager();
+	
+	@Override
+	public Pager getP() {
+		return DUMMY;
+	}
+
+	@Override
+	public void setP(Pager p) {
+	}
+
+	// short name for Csv/Json/Xml
+	public Pager getO() {
+		return getPager();
+	}
+
+	public void setO(Pager p) {
+		setPager(p);
+	}
+}
