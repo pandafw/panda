56e0c85f5073f1ac098404e961e3d97234509e43
Frank Wang <yf.frank.wang@outlook.com>
2016-11-09 21:33:13 +0900

    add zip file type

--- src/main/java/panda/io/FileType.java
+++ src/main/java/panda/io/FileType.java
@@ -6,4 +6,7 @@ public class FileType {
 	public static final String TXT = "txt";
 	public static final String XLS = "xls";
 	public static final String XLSX = "xlsx";
+	public static final String ZIP = "zip";
+	public static final String GZIP = "gzip";
+	public static final String GZ = "gz";
 }
