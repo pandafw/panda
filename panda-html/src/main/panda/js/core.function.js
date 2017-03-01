69e9a5ecfcd3297f9b45650639569e20a828c54c
Frank Wang <yf.frank.wang@outlook.com>
2017-03-01 21:31:37 +0900

    update js function methods

--- panda-html/src/main/panda/js/core.function.js
+++ panda-html/src/main/panda/js/core.function.js
@@ -10,12 +10,12 @@ function panda_call(f, p) {
 	}
 }
 
-if (typeof Function.prototype.createCallback != "function") {
+if (typeof Function.prototype.callback != "function") {
 	/**
 	 * Creates a callback that passes arguments[0], arguments[1], arguments[2], ...
-	 * Call directly on any function. Example: <code>myFunction.createCallback(arg1, arg2)</code>
+	 * Call directly on any function. Example: <code>myFunction.callback(arg1, arg2)</code>
 	 * Will create a function that is bound to those 2 args. <b>If a specific scope is required in the
-	 * callback, use {@link #createDelegate} instead.</b> The function returned by createCallback always
+	 * callback, use {@link #delegate} instead.</b> The function returned by callback always
 	 * executes in the window scope.
 	 * <p>This method is required when you want to pass arguments to a callback function.  If no arguments
 	 * are needed, you can simply pass a reference to the function as a callback (e.g., callback: myFn).
@@ -30,12 +30,12 @@ if (typeof Function.prototype.createCallback != "function") {
 		new Ext.Button({
 			text: 'Say Hi',
 			renderTo: Ext.getBody(),
-			handler: sayHi.createCallback('Fred')
+			handler: sayHi.callback('Fred')
 		});
 		</code></pre>
 	 * @return {Function} The new function
 	 */
-	Function.prototype.createCallback = function(/*args...*/){
+	Function.prototype.callback = function(/*args...*/){
 		// make args available, in function below
 		var args = arguments;
 		var method = this;
@@ -45,10 +45,31 @@ if (typeof Function.prototype.createCallback != "function") {
 	};
 }
 
-if (typeof Function.prototype.createDelegate != "function") {
+if (typeof Function.prototype.bind != "function") {
+	/**
+	 * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+	 * 
+	 * Syntax:
+	 * <pre><code>
+		fun.bind(thisArg[, arg1[, arg2[, ...]]])
+		</code></pre>
+	 * @return {Function} The new function
+	 */
+	Function.prototype.bind = function(/*args...*/){
+		// make args available, in function below
+		var scope = arguments[0] || window;
+		var args = Array.prototype.slice.call(arguments, 1);
+		var method = this;
+		return function() {
+			return method.apply(scope, args);
+		};
+	};
+}
+
+if (typeof Function.prototype.delegate != "function") {
 	/**
 	 * Creates a delegate (callback) that sets the scope to obj.
-	 * Call directly on any function. Example: <code>this.myFunction.createDelegate(this, [arg1, arg2])</code>
+	 * Call directly on any function. Example: <code>this.myFunction.delegate(this, [arg1, arg2])</code>
 	 * Will create a function that is automatically scoped to obj so that the <tt>this</tt> variable inside the
 	 * callback points to obj. Example usage:
 	 * <pre><code>
@@ -56,44 +77,38 @@ if (typeof Function.prototype.createDelegate != "function") {
 			// Note this use of "this.text" here.  This function expects to
 			// execute within a scope that contains a text property.  In this
 			// example, the "this" variable is pointing to the btn object that
-			// was passed in createDelegate below.
+			// was passed in delegate below.
 			alert('Hi, ' + name + '. You clicked the "' + this.text + '" button.');
 		}
 
-		var btn = new Ext.Button({
-			text: 'Say Hi',
-			renderTo: Ext.getBody()
+		var btn = new Button({
+			text: 'Say Hi'
 		});
 
 		// This callback will execute in the scope of the
 		// button instance. Clicking the button alerts
 		// "Hi, Fred. You clicked the "Say Hi" button."
-		btn.on('click', sayHi.createDelegate(btn, ['Fred']));
+		btn.on('click', sayHi.delegate(btn, ['Fred']));
 		</code></pre>
 	 * @param {Object} obj (optional) The object for which the scope is set
 	 * @param {Array} args (optional) Overrides arguments for the call. (Defaults to the arguments passed by the caller)
-	 * @param {Boolean/Number} appendArgs (optional) if True args are appended to call args instead of overriding,
-	 *											   if a number the args are inserted at the specified position
+	 * @param {Boolean} appendArgs (optional) if True args are appended to call args instead of overriding
 	 * @return {Function} The new function
 	 */
-	Function.prototype.createDelegate = function(obj, args, appendArgs) {
+	Function.prototype.delegate = function(obj, args, appendArgs) {
 		var method = this;
 		return function() {
 			var callArgs = args || arguments;
-			if(appendArgs === true){
+			if (appendArgs === true) {
 				callArgs = Array.prototype.slice.call(arguments, 0);
 				callArgs = callArgs.concat(args);
-			}else if(typeof appendArgs == "number"){
-				callArgs = Array.prototype.slice.call(arguments, 0); // copy arguments first
-				var applyArgs = [appendArgs, 0].concat(args); // create method call params
-				Array.prototype.splice.apply(callArgs, applyArgs); // splice them in
 			}
 			return method.apply(obj || window, callArgs);
 		};
 	};
 }
 
-if (typeof Function.prototype.defer != "function") {
+if (typeof Function.prototype.delay != "function") {
 	/**
 	 * Calls this function after the number of millseconds specified, optionally in a specific scope. Example usage:
 	 * <pre><code>
@@ -105,24 +120,22 @@ if (typeof Function.prototype.defer != "function") {
 		sayHi('Fred');
 
 		// executes after 2 seconds:
-		sayHi.defer(2000, this, ['Fred']);
+		sayHi.delay(2000, this, ['Fred']);
 
 		// this syntax is sometimes useful for deferring
 		// execution of an anonymous function:
 		(function(){
 			alert('Anonymous');
-		}).defer(100);
+		}).delay(100);
 		</code></pre>
 	 * @param {Number} millis The number of milliseconds for the setTimeout call (if 0 the function is executed immediately)
 	 * @param {Object} obj (optional) The object for which the scope is set
 	 * @param {Array} args (optional) Overrides arguments for the call. (Defaults to the arguments passed by the caller)
-	 * @param {Boolean/Number} appendArgs (optional) if True args are appended to call args instead of overriding,
-	 *											   if a number the args are inserted at the specified position
 	 * @return {Number} The timeout id that can be used with clearTimeout
 	 */
-	Function.prototype.defer = function(millis, obj, args, appendArgs) {
-		var fn = this.createDelegate(obj, args, appendArgs);
-		if(millis){
+	Function.prototype.delay = function(millis, obj, args) {
+		var fn = this.delegate(obj, args);
+		if (millis) {
 			return setTimeout(fn, millis);
 		}
 		fn();
@@ -130,42 +143,7 @@ if (typeof Function.prototype.defer != "function") {
 	};
 }
 
-if (typeof Function.prototype.createSequence != "function") {
-	/**
-	 * Create a combined function call sequence of the original function + the passed function.
-	 * The resulting function returns the results of the original function.
-	 * The passed fcn is called with the parameters of the original function. Example usage:
-	 * <pre><code>
-		var sayHi = function(name){
-			alert('Hi, ' + name);
-		}
-
-		sayHi('Fred'); // alerts "Hi, Fred"
-
-		var sayGoodbye = sayHi.createSequence(function(name){
-			alert('Bye, ' + name);
-		});
-
-		sayGoodbye('Fred'); // both alerts show
-		</code></pre>
-	 * @param {Function} fcn The function to sequence
-	 * @param {Object} scope (optional) The scope of the passed fcn (Defaults to scope of original function or window)
-	 * @return {Function} The new function
-	 */
-	Function.prototype.createSequence = function(fcn, scope) {
-		if(typeof fcn != "function"){
-			return this;
-		}
-		var method = this;
-		return function() {
-			var retval = method.apply(this || window, arguments);
-			fcn.apply(scope || this || window, arguments);
-			return retval;
-		};
-	};
-}
-
-if (typeof Function.prototype.createInterceptor != "function") {
+if (typeof Function.prototype.precall != "function") {
 	/**
 	 * Creates an interceptor function. The passed fcn is called before the original one. If it returns false,
 	 * the original one is not called. The resulting function returns the results of the original function.
@@ -179,7 +157,7 @@ if (typeof Function.prototype.createInterceptor != "function") {
 
 		// create a new function that validates input without
 		// directly modifying the original function:
-		var sayHiToFriend = sayHi.createInterceptor(function(name){
+		var sayHiToFriend = sayHi.precall(function(name){
 			return name == 'Brian';
 		});
 
@@ -190,7 +168,7 @@ if (typeof Function.prototype.createInterceptor != "function") {
 	 * @param {Object} scope (optional) The scope of the passed fcn (Defaults to scope of original function or window)
 	 * @return {Function} The new function
 	 */
-	Function.prototype.createInterceptor = function(fcn, scope) {
+	Function.prototype.precall = function(fcn, scope) {
 		if(typeof fcn != "function"){
 			return this;
 		}
@@ -206,3 +184,37 @@ if (typeof Function.prototype.createInterceptor != "function") {
 	};
 }
 
+if (typeof Function.prototype.postcall != "function") {
+	/**
+	 * Create a combined function call sequence of the original function + the passed function.
+	 * The resulting function returns the results of the original function.
+	 * The passed fcn is called with the parameters of the original function. Example usage:
+	 * <pre><code>
+		var sayHi = function(name){
+			alert('Hi, ' + name);
+		}
+
+		sayHi('Fred'); // alerts "Hi, Fred"
+
+		var sayGoodbye = sayHi.postcall(function(name){
+			alert('Bye, ' + name);
+		});
+
+		sayGoodbye('Fred'); // both alerts show
+		</code></pre>
+	 * @param {Function} fcn The function to sequence
+	 * @param {Object} scope (optional) The scope of the passed fcn (Defaults to scope of original function or window)
+	 * @return {Function} The new function
+	 */
+	Function.prototype.postcall = function(fcn, scope) {
+		if (typeof fcn != "function") {
+			return this;
+		}
+		var method = this;
+		return function() {
+			var retval = method.apply(this || window, arguments);
+			fcn.apply(scope || this || window, arguments);
+			return retval;
+		};
+	};
+}
