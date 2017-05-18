b88f09beed551d40c3cc4840b787227f88788a5f
Frank Wang <yf.frank.wang@outlook.com>
2017-05-18 21:58:55 +0900

    add AppMailer, WebMailer

--- panda-rail/src/main/java/panda/app/util/AppActionAssist.java
+++ panda-rail/src/main/java/panda/app/util/AppActionAssist.java
@@ -9,7 +9,6 @@ import java.util.Map.Entry;
 import panda.app.auth.AppAuthenticator;
 import panda.app.auth.IUser;
 import panda.app.constant.RES;
-import panda.app.constant.SET;
 import panda.app.constant.VAL;
 import panda.app.entity.ICreate;
 import panda.app.entity.IStatus;
@@ -21,8 +20,6 @@ import panda.cast.Castors;
 import panda.ioc.Scope;
 import panda.ioc.annotation.IocBean;
 import panda.ioc.annotation.IocInject;
-import panda.lang.Charsets;
-import panda.lang.Exceptions;
 import panda.lang.Locales;
 import panda.lang.Numbers;
 import panda.lang.Strings;
@@ -36,9 +33,6 @@ import panda.mvc.util.ActionAssist;
 import panda.mvc.util.MvcURLBuilder;
 import panda.mvc.util.StateProvider;
 import panda.mvc.view.ftl.FreemarkerHelper;
-import panda.net.mail.Email;
-import panda.net.mail.EmailClient;
-import panda.net.mail.EmailException;
 
 import freemarker.template.TemplateException;
 
@@ -393,139 +387,4 @@ public class AppActionAssist extends ActionAssist implements AccessControler {
 		getState().saveState("limit", pager.getLimit());
 	}
 	
-	//-------------------------------------------------------------
-	// Template mail
-	//
-	/**
-	 * send email
-	 * @param email email
-	 * @param tpl template name
-	 * @param model data
-	 */
-	public void sendTemplateMail(Email email, String tpl, Object model) throws EmailException {
-		String subject = "";
-		String content;
-		try {
-			content = freemarker.execTemplate(tpl, model);
-		}
-		catch (Exception e) {
-			throw Exceptions.wrapThrow(e);
-		}
-
-		int cr = content.indexOf('\n');
-		if (cr > 0) {
-			subject = Strings.strip(content.substring(0, cr));
-			content = content.substring(cr + 1);
-		}
-
-		email.setSubject(subject);
-		email.setMessage(content);
-
-		sendMail(email);
-	}
-
-	/**
-	 * send email
-	 * @param email email
-	 * @param name template name
-	 */
-	public void sendTemplateMail(Email email, String name) throws EmailException {
-		sendTemplateMail(email, name, null);
-	}
-	
-	/**
-	 * send email
-	 * @param email email
-	 * @throws EmailException if an email error occurs
-	 */
-	public void sendMail(Email email) throws EmailException {
-		if (email.getSender() == null) {
-			String s = getMailSetting(SET.MAIL_SENDER, null);
-			if (Strings.isNotEmpty(s)) {
-				email.setSender(s);
-			}
-		}
-
-		if (email.getFrom() == null) {
-			email.setFrom(getMailSetting(SET.MAIL_FROM, null));
-		}
-
-		String charset = getMailSetting(SET.MAIL_CHARSET, Charsets.UTF_8);
-		if (Strings.isNotEmpty(charset)) {
-			email.setCharset(charset);
-		}
-
-		EmailClient client = new EmailClient();
-		String helo = getMailSetting(SET.MAIL_SMTP_HELO, null);
-		if (Strings.isNotEmpty(helo)) {
-			client.setHelo(helo);
-		}
-		
-		String host = getMailSetting(SET.MAIL_SMTP_HOST, null);
-		if (Strings.isNotEmpty(host)) {
-			client.setHost(host);
-		}
-		
-		int port = getMailSettingAsInt(SET.MAIL_SMTP_PORT, 0);
-		if (port > 0) {
-			client.setPort(port);
-		}
-
-		if (getMailSettingAsBoolean(SET.MAIL_SMTP_SSL, false)) {
-			client.setSsl(true);
-		}
-
-		if (!getMailSettingAsBoolean(SET.MAIL_SMTP_TLS, true)) {
-			client.setTls(false);
-		}
-
-		int timeout = getMailSettingAsInt(SET.MAIL_SMTP_CONN_TIMEOUT, 0);
-		if (timeout > 0) {
-			client.setConnectTimeout(timeout);
-		}
-
-		timeout = getMailSettingAsInt(SET.MAIL_SMTP_SEND_TIMEOUT, 0);
-		if (timeout > 0) {
-			client.setDefaultTimeout(timeout);
-		}
-
-//		client.setTLS(getMailSettingAsBoolean(SC.MAIL_SMTP_TLS, false));
-		
-		String username = getMailSetting(SET.MAIL_SMTP_USER, null);
-		if (Strings.isNotEmpty(username)) {
-			client.setUsername(username);
-			client.setPassword(getMailSetting(SET.MAIL_SMTP_PASSWORD, ""));
-		}
-		
-		String bounce = getMailSetting(SET.MAIL_SMTP_BOUNCE, null);
-		if (Strings.isNotEmpty(bounce)) {
-//			email.setBounceAddress(bounce);
-		}
-
-		client.send(email);
-	}
-	
-	protected String getMailSetting(String key, String def) {
-		String s = context.getText().getText(key, null);
-		if (Strings.isNotEmpty(s)) {
-			return s;
-		}
-		return settings.getProperty(key, def);
-	}
-	
-	protected boolean getMailSettingAsBoolean(String key, boolean def) {
-		String v = Strings.stripToNull(getMailSetting(key, null));
-		if (Strings.isEmpty(v)) {
-			return def;
-		}
-		return (Boolean)Castors.scast(v, boolean.class);
-	}
-	
-	protected int getMailSettingAsInt(String key, int def) {
-		String v = Strings.stripToNull(getMailSetting(key, null));
-		if (Strings.isEmpty(v)) {
-			return def;
-		}
-		return (Integer)Castors.scast(v, int.class);
-	}
 }
---
+++ panda-rail/src/main/java/panda/app/util/AppMailer.java
@@ -0,0 +1,164 @@
+package panda.app.util;
+
+import panda.app.constant.SET;
+import panda.cast.Castors;
+import panda.ioc.Scope;
+import panda.ioc.annotation.IocBean;
+import panda.ioc.annotation.IocInject;
+import panda.lang.Charsets;
+import panda.lang.Exceptions;
+import panda.lang.Strings;
+import panda.mvc.ActionContext;
+import panda.mvc.view.ftl.FreemarkerHelper;
+import panda.net.mail.Email;
+import panda.net.mail.EmailClient;
+import panda.net.mail.EmailException;
+
+@IocBean(scope=Scope.REQUEST)
+public class AppMailer {
+	@IocInject
+	protected ActionContext context;
+
+	@IocInject
+	protected AppSettings settings;
+
+	@IocInject
+	protected FreemarkerHelper freemarker;
+	
+	//-------------------------------------------------------------
+	// Template mail
+	//
+	/**
+	 * send email
+	 * @param email email
+	 * @param tpl template name
+	 * @param model data
+	 */
+	public void sendTemplateMail(Email email, String tpl, Object model) throws EmailException {
+		String subject = "";
+		String content;
+		try {
+			content = freemarker.execTemplate(tpl, model);
+		}
+		catch (Exception e) {
+			throw Exceptions.wrapThrow(e);
+		}
+
+		int cr = content.indexOf('\n');
+		if (cr > 0) {
+			subject = Strings.strip(content.substring(0, cr));
+			content = content.substring(cr + 1);
+		}
+
+		email.setSubject(subject);
+		email.setMessage(content);
+
+		sendMail(email);
+	}
+
+	/**
+	 * send email
+	 * @param email email
+	 * @param name template name
+	 */
+	public void sendTemplateMail(Email email, String name) throws EmailException {
+		sendTemplateMail(email, name, null);
+	}
+	
+	/**
+	 * send email
+	 * @param email email
+	 * @throws EmailException if an email error occurs
+	 */
+	public void sendMail(Email email) throws EmailException {
+		if (email.getSender() == null) {
+			String s = getMailSetting(SET.MAIL_SENDER, null);
+			if (Strings.isNotEmpty(s)) {
+				email.setSender(s);
+			}
+		}
+
+		if (email.getFrom() == null) {
+			email.setFrom(getMailSetting(SET.MAIL_FROM, null));
+		}
+
+		String charset = getMailSetting(SET.MAIL_CHARSET, Charsets.UTF_8);
+		if (Strings.isNotEmpty(charset)) {
+			email.setCharset(charset);
+		}
+
+		EmailClient client = new EmailClient();
+		String helo = getMailSetting(SET.MAIL_SMTP_HELO, null);
+		if (Strings.isNotEmpty(helo)) {
+			client.setHelo(helo);
+		}
+		
+		String host = getMailSetting(SET.MAIL_SMTP_HOST, null);
+		if (Strings.isNotEmpty(host)) {
+			client.setHost(host);
+		}
+		
+		int port = getMailSettingAsInt(SET.MAIL_SMTP_PORT, 0);
+		if (port > 0) {
+			client.setPort(port);
+		}
+
+		if (getMailSettingAsBoolean(SET.MAIL_SMTP_SSL, false)) {
+			client.setSsl(true);
+		}
+
+		if (!getMailSettingAsBoolean(SET.MAIL_SMTP_TLS, true)) {
+			client.setTls(false);
+		}
+
+		int timeout = getMailSettingAsInt(SET.MAIL_SMTP_CONN_TIMEOUT, 0);
+		if (timeout > 0) {
+			client.setConnectTimeout(timeout);
+		}
+
+		timeout = getMailSettingAsInt(SET.MAIL_SMTP_SEND_TIMEOUT, 0);
+		if (timeout > 0) {
+			client.setDefaultTimeout(timeout);
+		}
+
+//		client.setTLS(getMailSettingAsBoolean(SC.MAIL_SMTP_TLS, false));
+		
+		String username = getMailSetting(SET.MAIL_SMTP_USER, null);
+		if (Strings.isNotEmpty(username)) {
+			client.setUsername(username);
+			client.setPassword(getMailSetting(SET.MAIL_SMTP_PASSWORD, ""));
+		}
+		
+		String bounce = getMailSetting(SET.MAIL_SMTP_BOUNCE, null);
+		if (Strings.isNotEmpty(bounce)) {
+//			email.setBounceAddress(bounce);
+		}
+
+		client.send(email);
+	}
+	
+	protected String getMailSetting(String key, String def) {
+		String s = context.getText().getText(key, null);
+		if (Strings.isNotEmpty(s)) {
+			return s;
+		}
+		return settings.getProperty(key, def);
+	}
+	
+	protected boolean getMailSettingAsBoolean(String key, boolean def) {
+		String v = Strings.stripToNull(getMailSetting(key, null));
+		if (Strings.isEmpty(v)) {
+			return def;
+		}
+		return (Boolean)Castors.scast(v, boolean.class);
+	}
+	
+	protected int getMailSettingAsInt(String key, int def) {
+		String v = Strings.stripToNull(getMailSetting(key, null));
+		if (Strings.isEmpty(v)) {
+			return def;
+		}
+		return (Integer)Castors.scast(v, int.class);
+	}
+
+}
