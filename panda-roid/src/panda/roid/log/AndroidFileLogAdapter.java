54e53070e59050f472a464adb8b9fc144df342a3
Frank Wang <yf.frank.wang@outlook.com>
2017-07-07 21:25:12 +0900

    change android file log (external) setting to (internal)

--- panda-roid/src/panda/roid/log/AndroidFileLogAdapter.java
+++ panda-roid/src/panda/roid/log/AndroidFileLogAdapter.java
@@ -1,27 +1,18 @@
 package panda.roid.log;
 
 import panda.io.FileNames;
-import panda.lang.Strings;
+import panda.lang.Booleans;
 import panda.log.impl.FileLogAdapter;
 import panda.roid.Androids;
 
 
 public class AndroidFileLogAdapter extends FileLogAdapter {
-	private Boolean ext;
-	private String tag;
+	private boolean internal;
 	
 	@Override
 	protected void setProperty(String name, String value) {
-		if ("tag".equalsIgnoreCase(name)) {
-			tag = Strings.isEmpty(value) ? getClass().getSimpleName() : value;
-		}
-		else if ("ext".equalsIgnoreCase(name)) {
-			if ("true".equalsIgnoreCase(value)) {
-				ext = true;
-			}
-			else if ("false".equalsIgnoreCase(value)) {
-				ext = false;
-			}
+		if ("internal".equalsIgnoreCase(name)) {
+			internal = Booleans.toBoolean(value);
 		}
 		else {
 			super.setProperty(name, value);
@@ -30,22 +21,11 @@ public class AndroidFileLogAdapter extends FileLogAdapter {
 
 	@Override
 	protected String getAbsolutePath() {
-		if (Boolean.FALSE.equals(ext)) {
-			if (Androids.getInternalStorageDirectory() != null) {
-				return FileNames.concat(Androids.getInternalStorageDirectory().getAbsolutePath(), path);
-			}
+		// Androids.init(context) should be called before Logs.getLog().
+
+		if (internal && Androids.getInternalStorageDirectory() != null) {
+			return FileNames.concat(Androids.getInternalStorageDirectory().getAbsolutePath(), path);
 		}
 		return FileNames.concat(Androids.getExternalStorageDirectory().getAbsolutePath(), path);
-	}
-	
-	@Override
-	protected void rollOver() {
-		try {
-			super.rollOver();
-		}
-		catch (RuntimeException e) {
-			android.util.Log.e(tag, getClass().getSimpleName() + ": " + e.getMessage(), e);
-			throw e;
-		}
 	}
 }
