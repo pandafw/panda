6049a453e65241d0c2e6b40a2b311897d12a8cba
Frank Wang <yf.frank.wang@outlook.com>
2017-08-03 23:48:18 +0900

    improve jquery.ui.scrollIntoView

--- panda-html/src/main/jquery/js/jquery-plugins.js
+++ panda-html/src/main/jquery/js/jquery-plugins.js
@@ -1930,9 +1930,31 @@ jQuery.jcookie = function(name, value, options) {
 	};
 })(jQuery);
 (function($) {
-	$.fn.scrollIntoView = function() {
-		if (this.length) {
-			this.get(0).scrollIntoView();
+	$.fn.scrollIntoView = function(c) {
+		if (!this.length) {
+			return this;
+		}
+		
+		var $e = this.first(), $w = $(window), eo = $e.offset();
+		var wh = $w.height(), ww = $w.width();
+		var st = $w.scrollTop(), sb = st + wh, sl = $w.scrollLeft(), sr = sl + ww;
+		var et = eo.top, eh = $e.outerHeight(), eb = et + eh;
+		var el = eo.left, ew = $e.outerWidth(), er = el + ew;
+		
+		var x = sl > er ? el : (sr < el ? (ew > ww ? el : el - (ww - ew)) : -1);
+		var y = st > eb ? et : (sb < et ? (eh >= wh ? et : et - (wh - eh)) : -1);
+		
+		if (x >= 0 || y >= 0) {
+			var ss = {};
+			if (x >= 0) { ss.scrollLeft = x; }
+			if (y >= 0) { ss.scrollTop = y; }
+			var $hb = $('html,body');
+			if (c) {
+				$hb.animate(ss, c);
+			}
+			else {
+				$hb.prop(ss);
+			}
 		}
 		return this;
 	};
