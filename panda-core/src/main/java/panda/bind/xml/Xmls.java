cc734cd7e3699ac22fc1f11533431ada5458c21f
Frank Wang <yf.frank.wang@outlook.com>
2018-06-26 21:21:29 +0900

    add Xmls.newXmlDeserializer()

--- panda-core/src/main/java/panda/bind/xml/Xmls.java
+++ panda-core/src/main/java/panda/bind/xml/Xmls.java
@@ -11,25 +11,31 @@ import panda.io.Streams;
  *
  */
 public abstract class Xmls {
-	public static <T> T fromXml(String xml, Type type) {
-		XmlDeserializer xd = new XmlDeserializer();
-		return xd.deserialize(xml, type);
-	}
 
-	public static <T> T fromXml(Reader xml, Type type) {
+	public static XmlDeserializer newXmlDeserializer() {
 		XmlDeserializer xd = new XmlDeserializer();
-		return xd.deserialize(xml, type);
-	}
-
-	public static <T> T fromXml(InputStream xml, String encoding, Type type) {
-		Reader r = Streams.toReader(xml, encoding);
-		XmlDeserializer xd = new XmlDeserializer();
-		return xd.deserialize(r, type);
+		return xd;
 	}
 
 	public static XmlSerializer newXmlSerializer() {
 		XmlSerializer xs = new XmlSerializer();
 		return xs;
+	}
+
+	public static <T> T fromXml(String xml, Type type) {
+		XmlDeserializer xd = newXmlDeserializer();
+		return xd.deserialize(xml, type);
+	}
+
+	public static <T> T fromXml(Reader xml, Type type) {
+		XmlDeserializer xd = newXmlDeserializer();
+		return xd.deserialize(xml, type);
+	}
+
+	public static <T> T fromXml(InputStream xml, String encoding, Type type) {
+		Reader r = Streams.toReader(xml, encoding);
+		XmlDeserializer xd = newXmlDeserializer();
+		return xd.deserialize(r, type);
 	}
 	
 	public static String toXml(Object value) {
