26d6a277f439cfa04e06099b26982e72c0684c62
Frank Wang <yf.frank.wang@outlook.com>
2018-09-22 09:48:38 +0900

    add commonly used file types

--- panda-core/src/main/java/panda/io/FileTypes.java
+++ panda-core/src/main/java/panda/io/FileTypes.java
@@ -2,12 +2,20 @@ package panda.io;
 
 public class FileTypes {
 	public static final String CSV = "csv";
+	public static final String DOC = "doc";
+	public static final String DOCX = "docx";
+	public static final String GIF = "gif";
+	public static final String GZ = "gz";
+	public static final String GZIP = "gzip";
+	public static final String JPG = "jpg";
+	public static final String JPEG = "jpeg";
 	public static final String PDF = "pdf";
+	public static final String PNG = "png";
+	public static final String PPT = "ppt";
+	public static final String PPTX = "pptx";
 	public static final String TSV = "tsv";
 	public static final String TXT = "txt";
 	public static final String XLS = "xls";
 	public static final String XLSX = "xlsx";
 	public static final String ZIP = "zip";
-	public static final String GZIP = "gzip";
-	public static final String GZ = "gz";
 }
