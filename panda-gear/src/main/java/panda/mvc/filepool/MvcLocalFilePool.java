b0f63ed7fb302a70f46411c0ff21efd86ee0240d
Frank Wang <yf.frank.wang@outlook.com>
2018-11-23 10:37:55 +0900

    append servlet context name to default file pool path

--- panda-gear/src/main/java/panda/mvc/filepool/MvcLocalFilePool.java
+++ panda-gear/src/main/java/panda/mvc/filepool/MvcLocalFilePool.java
@@ -1,13 +1,36 @@
 package panda.mvc.filepool;
 
+import java.io.File;
+
+import javax.servlet.ServletContext;
+
+import panda.io.FileNames;
+import panda.io.Files;
 import panda.ioc.annotation.IocBean;
 import panda.ioc.annotation.IocInject;
+import panda.lang.Strings;
 import panda.mvc.MvcConstants;
 import panda.vfs.FilePool;
 import panda.vfs.local.LocalFilePool;
 
-@IocBean(type=FilePool.class)
+@IocBean(type=FilePool.class, create="initialize")
 public class MvcLocalFilePool extends LocalFilePool {
+	@IocInject(required=false)
+	private ServletContext servlet;
+	
+	public void initialize() {
+		if (Strings.isEmpty(path)) {
+			String sub = null;
+			if (servlet != null) {
+				sub = FileNames.trimFileName(servlet.getServletContextName());
+			}
+			if (Strings.isEmpty(sub)) {
+				sub = "files";
+			}
+			path = new File(Files.getTempDirectory(), sub).getPath();
+		}
+	}
+	
 	/**
 	 * @param path the path to set
 	 */
