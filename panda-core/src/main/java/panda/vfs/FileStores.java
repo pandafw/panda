8772f4a33d1f49513f6da658983020f35e8e4bea
Frank Wang <yf.frank.wang@outlook.com>
2019-01-05 15:55:09 +0900

    add FileStores.copy()

--- panda-core/src/main/java/panda/vfs/FileStores.java
+++ panda-core/src/main/java/panda/vfs/FileStores.java
@@ -1,6 +1,33 @@
 package panda.vfs;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import panda.io.Streams;
+
 public abstract class FileStores {
+	public static void copy(FileItem src, File des) throws IOException {
+		InputStream in = src.open();
+		try {
+			Streams.copy(in, des);
+		}
+		finally {
+			Streams.safeClose(in);
+		}
+	}
+	
+	public static void copy(File src, FileItem des) throws IOException {
+		InputStream in = new FileInputStream(src);
+		try {
+			des.save(in);
+		}
+		finally {
+			Streams.safeClose(in);
+		}
+	}
+	
 	public static void safeDelete(FileItem f) {
 		if (f == null || !f.isExists()) {
 			return;
