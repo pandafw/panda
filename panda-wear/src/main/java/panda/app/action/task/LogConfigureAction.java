1998ff8cddf04b50943e08870a206d2809ea454c
Frank Wang <yf.frank.wang@outlook.com>
2019-01-17 23:52:21 +0900

    add LogConfigureAction to reload log.properties

---
+++ panda-wear/src/main/java/panda/app/action/task/LogConfigureAction.java
@@ -0,0 +1,39 @@
+package panda.app.action.task;
+
+import java.io.File;
+
+import panda.app.action.work.GenericSyncWorkAction;
+import panda.app.auth.Auth;
+import panda.app.constant.AUTH;
+import panda.io.Settings;
+import panda.lang.Systems;
+import panda.log.Log;
+import panda.log.Logs;
+import panda.mvc.annotation.At;
+
+
+/**
+ * configure log on log.properties modified
+ */
+@At("${!!task_path|||'/task'}/logconfigure")
+@Auth({ AUTH.LOCAL, AUTH.TOKEN, AUTH.SUPER })
+public class LogConfigureAction extends GenericSyncWorkAction {
+	private static final Log log = Logs.getLog(LogConfigureAction.class);
+	
+	@Override
+	protected void doExecute() {
+		String prop = Systems.getProperty(Logs.CONFIG, Logs.CONFIG);
+		File file = new File(prop);
+		
+		if (file.exists() && file.lastModified() > Logs.getConfigureTime()) {
+			log.info("Configure Logs: " + file.getAbsolutePath());
+			try {
+				Settings props = new Settings(prop);
+				Logs.configure(props);
+			}
+			catch (Exception e) {
+				log.error("Failed to configure logs " + file, e);
+			}
+		}
+	}
+}
