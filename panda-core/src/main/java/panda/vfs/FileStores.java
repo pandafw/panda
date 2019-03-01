efd9f4af92c1f7c538f01928a76e58e1fd5350b5
Frank Wang <yf.frank.wang@outlook.com>
2019-03-01 20:53:45 +0900

    add FileStores.toByteArrays(FileItem)

--- panda-core/src/main/java/panda/vfs/FileStores.java
+++ panda-core/src/main/java/panda/vfs/FileStores.java
@@ -2,12 +2,32 @@ package panda.vfs;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
 import panda.io.Streams;
 
 public abstract class FileStores {
+	/**
+	 * read file content to byte array
+	 * 
+	 * @param file file
+	 * @return byte array
+	 * @throws FileNotFoundException if file not found
+	 * @throws IOException in case of I/O errors
+	 */
+	public static byte[] toByteArray(FileItem file) throws FileNotFoundException, IOException {
+		InputStream fis = file.open();
+		try {
+			byte[] b = Streams.toByteArray(fis);
+			return b;
+		}
+		finally {
+			Streams.safeClose(fis);
+		}
+	}
+
 	public static void copy(FileItem src, File des) throws IOException {
 		InputStream in = src.open();
 		try {
