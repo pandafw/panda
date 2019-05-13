4453cd81904b1a752c176d2222a51f64fb40287a
Frank Wang <yf.frank.wang@outlook.com>
2019-05-13 11:30:41 +0900

    html: fix bug for page_sort_reverse, and set default unloadmask timeout 1s

--- panda-html/src/html/panda/html/panda/js/panda.js
+++ panda-html/src/html/panda/html/panda/js/panda.js
@@ -2515,17 +2515,17 @@ if (typeof(panda) == "undefined") { panda = {}; }
 if (typeof(panda) == "undefined") { panda = {}; }
 
 //------------------------------------------------------
-panda.loading = function() {
-	$('body').loadmask({ mask: false, window: true });
+panda.loading = function(timeout) {
+	$('body').loadmask({ mask: false, window: true, timeout: timeout || 1000 });
 };
 
-panda.page_sort_reverse = function(name, dir) {
-	return sl_sortn(name, dir.toLowerCase() == "asc" ? "desc" : "asc");
-};
 panda.page_sort = function(name, dir) {
 	panda.loading();
 	location.href = $.addQueryParams(location.href, { 's.c': name, 's.d': dir });
 	return false;
+};
+panda.page_sort_reverse = function(name, dir) {
+	return panda.page_sort(name, dir.toLowerCase() == "asc" ? "desc" : "asc");
 };
 panda.page_goto = function(s) {
 	panda.loading();
