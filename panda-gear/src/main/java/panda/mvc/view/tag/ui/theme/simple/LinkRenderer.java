c7fb6dbb0a17084e432d2d8c64c1641666199b84
Frank Wang <yf.frank.wang@outlook.com>
2020-03-25 16:01:08 +0900

    html: upgrade bootstrap 3.4.1

--- panda-gear/src/main/java/panda/mvc/view/tag/ui/theme/simple/LinkRenderer.java
+++ panda-gear/src/main/java/panda/mvc/view/tag/ui/theme/simple/LinkRenderer.java
@@ -26,7 +26,7 @@ public class LinkRenderer extends AbstractEndRenderer<Link> {
 	private static final String JQUERY3_BIND_PATH = JQUERY_BIND_BASE + JQUERY3_VERSION;
 
 	private static final String BOOTSTRAP_CDN_BASE = "https://netdna.bootstrapcdn.com/bootstrap/";
-	private static final String BOOTSTRAP3_VERSION = "3.4.0";
+	private static final String BOOTSTRAP3_VERSION = "3.4.1";
 	
 	private static final String FONTAWESOME_CDN_BASE = "https://netdna.bootstrapcdn.com/font-awesome/";
 	private static final String FONTAWESOME4_VERSION = "4.7.0";
