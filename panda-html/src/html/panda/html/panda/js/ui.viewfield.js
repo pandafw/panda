902a7ea5443b70ab1bdcd68a55e70a733312a667
Frank Wang <yf.frank.wang@outlook.com>
2020-07-31 16:10:53 +0900

    hook viewfield input change event instead of panda.viewfield()

--- panda-html/src/html/panda/html/panda/js/ui.viewfield.js
+++ panda-html/src/html/panda/html/panda/js/ui.viewfield.js
@@ -1,39 +1,19 @@
-if (typeof(panda) == "undefined") { panda = {}; }
-
-panda.viewfield = function(o) {
-	var api = {
-		el: $(o),
-		val: function(v, t) {
-			if (typeof(v) == 'undefined') {
-				return this.el.val();
-			}
-
-			var o = this.el.val();
-			this.el.val(v).next('.p-viewfield').text(v == '' ? '\u3000' : v);
-			if (t && o != v) {
-				this.el.trigger('change');
-			}
-			return this;
-		}
-	};
-	
-	return api;
-};
-
 (function($) {	
 	$(window).on('load', function () {
+		$('.p-viewfield').prev('input').change(function() {
+			var v = $(this).val();
+			$(this).next('.p-viewfield').text(v == '' ? '\u3000' : v);
+		});
+		
 		$('.p-viewfield[data-format="html"]').each(function() {
-			var $t = $(this);
 			$('<a href="#" class="p-vf-code"><i class="fa fa-code"></i></a>').appendTo($(this).parent());
 		});
 		
 		$('.p-vf-code').click(function() {
 			var $t = $(this);
-			$t.toggleClass('active');
-			
-			var $p = $t.parent();
+			var $p = $t.toggleClass('active').parent();
 			var v = $p.find('input').val();
-			var $d = $p.find('.p-viewfield').html($t.hasClass('active') ? v.escapePhtml() : v);
+			$p.find('.p-viewfield').html($t.hasClass('active') ? v.escapePhtml() : v);
 			return false;
 		});
 	});
