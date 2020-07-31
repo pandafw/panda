d2f350eac90e0ed7dfc5ddba3047aebe73648b4a
Frank Wang <yf.frank.wang@outlook.com>
2020-07-31 16:11:20 +0900

    add jquery.ui.formvals.js

--- panda-html/src/html/panda/html/jquery/js/jquery-plugins.js
+++ panda-html/src/html/panda/html/jquery/js/jquery-plugins.js
@@ -951,6 +951,39 @@ jQuery.jcookie = function(name, value, options) {
 	});
 })(jQuery);
 (function($) {
+	$.fn.vals = function(vs) {
+		if (vs) {
+			for (var n in vs) {
+				var v = vs[n];
+				this.find('input[name="' + n + '"]').each(function() {
+					var $t = $(this);
+					switch ($t.attr('type')) {
+					case 'button':
+					case 'file':
+					case 'submit':
+						break;
+					case 'checkbox':
+					case 'radio':
+						$t.prop('checked', $t.val() == v);
+						break;
+					default:
+						$t.changeValue(v);
+					}
+				});
+			}
+			return this;
+		}
+
+		var m = {}, a = this.serializeArray();
+		$.each(a, function(i, v) {
+			m[v.name] = v.value;
+		});
+		return m;
+	};
+
+})(jQuery);
+
+(function($) {
 	function clearMaskTimeout($el) {
 		//if this element has delayed mask scheduled then remove it
 		var t = $el.data("_mask_timeout");
