8d652d1e155471c30f578389459310bd94e34416
Frank Wang <yf.frank.wang@outlook.com>
2020-12-03 09:39:20 +0900

    update jndi lookup

--- panda-core/src/main/java/panda/util/JndiLookup.java
+++ panda-core/src/main/java/panda/util/JndiLookup.java
@@ -6,15 +6,16 @@ import javax.naming.NamingException;
 
 public class JndiLookup {
 	public static Object lookup(String name) throws NamingException {
+		Context ctx = new InitialContext();
 		try {
-			return ((Context)new InitialContext().lookup("java:comp/env")).lookup(name);
+			return ((Context)ctx.lookup("java:comp/env")).lookup(name);
 		}
 		catch (NamingException e) {
 			try {
-				return new InitialContext().lookup(name);
+				return ctx.lookup(name);
 			}
 			catch (NamingException e2) {
-				return ((Context)new InitialContext().lookup("java:/comp/env")).lookup(name);
+				return ((Context)ctx.lookup("java:/comp/env")).lookup(name);
 			}
 		}
 	}
